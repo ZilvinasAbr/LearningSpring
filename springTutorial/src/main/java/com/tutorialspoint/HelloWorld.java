5f27ce23127355b215bbf852e7908f50d11b54a0
Zilvinas Abromavicius <Zilvinas.Abromavicius@devbridge.com>
2017-11-06 14:07:28 +0200

    Bean Post Processors. Example works, destroy gets called somehow.

--- springTutorial/src/main/java/com/tutorialspoint/HelloWorld.java
+++ springTutorial/src/main/java/com/tutorialspoint/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     private String message;
 
     public void setMessage(String message){
-        this.message = message;
+        this.message  = message;
     }
     public void getMessage(){
         System.out.println("Your Message : " + message);
@@ -12,7 +12,7 @@ public class HelloWorld {
     public void init(){
         System.out.println("Bean is going through init.");
     }
-    public void destroy() {
+    public void destroy(){
         System.out.println("Bean will destroy now.");
     }
 }
